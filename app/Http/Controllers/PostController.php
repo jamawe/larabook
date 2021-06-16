301cb96fc92e41f6410522cb3b2c54ea2593ebc8
Jana Wernick <jana.wernick@gmail.com>
2021-06-16 18:23:58 +0200

    a_user_can_post_a_text_post test in PostToTimelineTest working

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function store() {
+
+        $data = request()->validate([
+            'data.attributes.body' => '',
+        ]);
+
+        // Bc the relationship not yet exists Post::create not possible, but request()->user()->posts()->create($data) works
+        $post = request()->user()->posts()->create($data['data']['attributes']);
         
-        return response([], 201);
+        return response([
+
+            'data' => [
+                'type' => 'posts',
+                'post_id' => $post->id,
+                'attributes' => [
+                    'body' => $post->body,
+                ],
+            ],
+            'links' => [
+                'self' => url('/posts/'.$post->id),
+            ]
+
+        ], 201);
     }
 }
--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -26,6 +26,7 @@ class PostToTimelineTest extends TestCase
 
         $this->actingAs($user = User::factory()->create(), 'api');
 
+        // Properly formatted request
         $response = $this->post('/api/posts', [
             'data' => [
                 'type' => 'posts',
@@ -38,6 +39,24 @@ class PostToTimelineTest extends TestCase
         // If post was successfully created it must exist in database
         $post = Post::first();
 
-        $response->assertStatus(201);
+        // Assert 1 if all of the posts are fetched (check if there actually is a post)
+        $this->assertCount(1, Post::all()); // Test result: Failed asserting that actual size 0 matches expected size 1. -- no post
+
+        // Define what is expected in the response
+        $this->assertEquals($user->id, $post->user_id);
+        $this->assertEquals('Testing Body', $post->body);
+        $response->assertStatus(201)
+            ->assertJson([ // Properly formatted response
+                'data' => [
+                    'type' => 'posts',
+                    'post_id' => $post->id,
+                    'attributes' => [
+                        'body' => 'Testing Body',
+                    ],
+                ],
+                'links' => [
+                    'self' => url('/posts/'.$post->id),
+                ]
+            ]);
     }
 }
