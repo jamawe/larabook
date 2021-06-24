5a04946406974b265e7f53c15475398ea91762e7
Jana Wernick <jana.wernick@gmail.com>
2021-06-24 11:37:34 +0200

    Created test for retrieving user profiles

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,15 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
 
-    // Any route inside callback will be protected by api
-    // Since api.php no 'api' prefix in url param needed
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::apiResources([
+        'posts' => PostController::class,
+        'users' => UserController::class,
+    ]);
 
-    Route::get('/posts', [PostController::class, 'index']);
+    // Route::get('/posts', [PostController::class, 'index']);
 
-    Route::post('/posts', [PostController::class, 'store']);
+    // Route::post('/posts', [PostController::class, 'store']);
 
 });
 
