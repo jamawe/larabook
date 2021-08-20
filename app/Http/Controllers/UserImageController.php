184d721dd35586d85a63c5e98f0c73b6357eea2c
Jana Wernick <jana.wernick@gmail.com>
2021-08-20 18:16:42 +0200

    Lost commit

---
+++ app/Http/Controllers/UserImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserImage;
+use App\Http\Resources\UserImage as UserImageResource;
+
+class UserImageController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'image' => '',
+            'width' => '',
+            'height' => '',
+            'location' => '',
+        ]);
+
+        $image = $data['image']->store('user-images', 'public');
+
+        $userImage = auth()->user()->images()->create([
+            'path' => $image,
+            'width' => $data['width'],
+            'height' => $data['height'],
+            'location' => $data['location'],
+        ]);
+
+        return new UserImageResource($userImage);
+        // return response([], 201);
+    }
+}
